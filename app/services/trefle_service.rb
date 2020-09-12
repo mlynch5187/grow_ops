242753f32d03a52b32de1a282de33cfefb6da685
Nick Edwin <nickedwin85@gmail.com>
2020-09-12 17:02:41 -0600

    Updated to view and need to update more api call filters
    Co-authored-by: Ryan Laleh <33795357+RyN21@users.noreply.github.com>
    Co-authored-by: Chandler Hulstrom <62038636+Chulstro@users.noreply.github.com>
    Co-authored-by: Michael Alex Lynch <53200401+mlynch5187@users.noreply.github.com>

--- app/services/trefle_service.rb
+++ app/services/trefle_service.rb
@@ -1,12 +1,11 @@
 class TrefleService
-
   def filter(ph, light)
     plants = []
     page_number = 1
       until plants.length > 50 do
       if ph == 0.0
         light_response = conn.get("/api/v1/plants?range[light]=#{light - 2},#{light + 2}&token=#{ENV['TREFLE_ID']}&page=#{page_number}")
-        plants_info << JSON.parse(light_response.body, symbolize_names: true)
+        plants_info = JSON.parse(light_response.body, symbolize_names: true)
 
         plants_info[:data].each do |plant|
           plants << plant
@@ -14,20 +13,17 @@ class TrefleService
 
         page_number += 1
       else
+        ### lets add more filters ... height less than 300cm (10ft)..., poisonous?
         ph_response = conn.get("/api/v1/plants?range[ph_minimum]=#{ph - 2}&,range[ph_maximum]=#{ph + 2}&token=#{ENV['TREFLE_ID']}&page=#{page_number}")
         plants_info = JSON.parse(ph_response.body, symbolize_names: true)
 
         plants_info[:data].each do |plant|
-          individual_plant_response = conn.get("#{plant[:links][:self]}?token=#{ENV['TREFLE_ID']}")
-          individual_plant_info = JSON.parse(individual_plant_response.body, symbolize_names: true)
-            plant_object = Plant.new(individual_plant_info)
-          plants << plant_object
+          plants << plant
         end
 
         page_number += 1
       end
     end
-    require"pry"; binding.pry
     plants
   end
 
@@ -37,3 +33,10 @@ class TrefleService
     Faraday.new(url: 'https://trefle.io')
   end
 end
+
+# plants.map do |plant|
+#   individual_plant_response = conn.get("#{plant[:links][:self]}?token=#{ENV['TREFLE_ID']}")
+#   individual_plant_info = JSON.parse(individual_plant_response.body, symbolize_names: true)
+#   Plant.new(individual_plant_info)
+#   # plants << plant_object
+# end
